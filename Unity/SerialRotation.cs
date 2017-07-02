4dd0c09f651638718f0cf8767a657503f0e171e1
Yilj <nigk@web.de>
2017-07-02 22:24:30 +0200

    Adding Unity Scripts

---
+++ Unity/SerialRotation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class SerialRotation : MonoBehaviour {
+
+    SerialPort stream = new SerialPort("COM8", 9600);
+
+    void Start()
+    {
+        stream.Open();
+        stream.ReadTimeout = 25;
+    }
+
+    void Update()
+    {
+        string input = stream.ReadLine();
+        string[] inputs = input.Split(',');
+        float steering = int.Parse(inputs[0]);
+        float gas = int.Parse(inputs[1]);
+        GameObject.Find("AutoDownload").GetComponent<AudioSource>().pitch = (180 - gas) / 180;
+        Debug.Log(180 - gas);
+        //Debug.Log(value);
+        steering = (steering - 240) / (30 - 240) * (270 - 90) + 180;
+        transform.eulerAngles= new Vector3(steering, 180, 0);
+        stream.BaseStream.Flush();
+    }
+}
---
+++ Unity/Webacme.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Webacme : MonoBehaviour {
+    WebCamTexture webTexture;
+
+	void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        for (int i = 0; i < devices.Length; i++)
+            Debug.Log(devices[i].name);
+        webTexture = new WebCamTexture("SMI Grabber Device", 720, 480, 120);
+        GetComponent<MeshRenderer>().material.mainTexture = webTexture;
+        webTexture.Play();
+	}
+	
+	void Update()
+    {
+
+	}
+}
